18d7a191f07685ad29ee61c47cf6a71c6ab18852
BrainCell-404 <braincellno404@gmail.com>
2022-10-20 20:58:06 +0700

    feat: initial setup

---
+++ src/Pages/Mainpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Component1 from "../Components/Component1";
+
+const Mainpage = () => {
+  return (
+    <>
+      <Component1 />
+      <p>Test paragraph</p>
+    </>
+  );
+};
+
+export default Mainpage;
