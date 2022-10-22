59cc1348229e48e582c8f14476343c8b83118cde
BrainCell-404 <braincellno404@gmail.com>
2022-10-22 11:15:53 +0700

    feat : create Latest news card

---
+++ src/Components/Latest News/LatestNews.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+
+const LatestNews = () => {
+  return (
+    <div className="px-20 py-12">
+      <h1 className="my-5 text-4xl font-bold">Latest News</h1>
+      <div className="content grid grid-cols-4 gap-x-5 gap-y-8">
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center basis-3/12">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-4">
+              <h5 className="text-black text-base font-bold mb-2 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                eius.
+              </h5>
+              <div className="bot-info flex flex-row justify-around mt-10">
+                <span className="text-base opacity-40">Created Date</span>
+                <span className="text-base opacity-40">Created By</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LatestNews;
