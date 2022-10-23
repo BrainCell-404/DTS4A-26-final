dde304b6e38e72607c153f06fd6a6b9c50ec6d33
BrainCell-404 <braincellno404@gmail.com>
2022-10-23 18:46:02 +0700

    feat: make store for fetch article from user input via navbar(only navbar ~ 50%)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import UserSearch from "./Pages/UserSearch";
 
-import './styles/output.css'
+import "./styles/output.css";
 
 import Mainpage from "./Pages/Mainpage";
-
 
 function App() {
   return (
     <div className="App">
       <Mainpage />
+      {/* <UserSearch /> */}
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logoutIcon from "../../Assets/log-out.png";
 
+import storesSearchArticle, { selectSearchArticle } from "../../Stores/Store3";
+
 const Navbar = () => {
+  const [userInput, setUserInput] = useState("");
+
+  // const testArticle = storesSearchArticle(selectArticles);
+  const fetchArticle = storesSearchArticle(selectSearchArticle);
+
+  const onChangeHandler = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const searchArticle = (e) => {
+    if (e.key === "Enter") {
+      console.log("article yang dicari", userInput);
+      fetchArticle(userInput);
+    }
+  };
+
   return (
     <nav className="font-sans flex flex-row justify-between bg-white shadow w-full items-center px-5 py-3 lg:px-20 lg:py-8 ">
       <div className=" sm:mb-0 items-center">
@@ -17,6 +35,9 @@ const Navbar = () => {
             className="px-4 py-3 md:p-2 rounded border-neutral-600"
             type="text"
             placeholder="Search Article"
+            value={userInput}
+            onChange={onChangeHandler}
+            onKeyPress={searchArticle}
           />
         </div>
         <a href="..." className="text-black font-bold ">
